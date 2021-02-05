5846784c0d84f75d37f7089ae779254e963ee439
srikanth <sy99b@mail.umkc.edu>
2021-02-05 13:59:37 -0600

    updated

--- code/Prob3.py
+++ code/Prob3.py
@@ -5,15 +5,3 @@ row_maxes = np.amax(a, axis=1)
 b = np.where(a == row_maxes.reshape(-1,1),0,a)
 print("replaced highest row value by zero")
 print(b)
-"""j=0
-#print(max_index_row)
-for i in max_index_row:
-    a[j][i] = 0
-    j+=1
-print(a)
-#result = np.where(np.amax(a, axis=1),[3])
-index = np.max(a, axis=1)
-row_maxes = a.max(axis=1)
-#np.where(a == row_maxes, 1, 0)
-print(np.where(index,0,a))
-print(a)"""
